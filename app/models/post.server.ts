de58878692953ec2aec77bc7d8ebda548b8cee0a
Dylan L <dylancblarocque@gmail.com>
2023-03-24 10:13:53 -0700

    Model data with prisma

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,19 +1,10 @@
+import { prisma } from "~/db.server";
+
 // type Post = {
 //   slug: string;
 //   title: string;
 // };
 
 export async function getPosts() {
-  const posts = [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
-
-  return posts;
+  return prisma.post.findMany();
 }
