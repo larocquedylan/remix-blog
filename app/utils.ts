8034e250f2b0410c08ed3a341dcc64b43b8af6f9
Dylan L <dylancblarocque@gmail.com>
2023-03-26 08:21:54 -0700

    create useOptionalAdminUser util

--- app/utils.ts
+++ app/utils.ts
@@ -1,5 +1,6 @@
 import { useMatches } from "@remix-run/react";
 import { useMemo } from "react";
+import { isNull } from "util";
 
 import type { User } from "~/models/user.server";
 
@@ -64,6 +65,13 @@ export function useUser(): User {
     );
   }
   return maybeUser;
+}
+
+export function useOptionalAdminUser() {
+  const user = useOptionalUser();
+  if (!user) return null;
+  if (user.email !== ENV.ADMIN_EMAIL) return null;
+  return user;
 }
 
 export function validateEmail(email: unknown): email is string {
