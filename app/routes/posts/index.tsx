adc754746b08e5057b9cf73b1ad7cc0d0484b953
Dylan L <dylancblarocque@gmail.com>
2023-03-26 08:22:17 -0700

    hide admin btn if not admin user

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -2,6 +2,7 @@ import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { getPosts } from "~/models/post.server";
+import { useOptionalAdminUser, useOptionalUser } from "~/utils";
 
 // need to add type because `json` in the loader is a generic so we need to tell it what type of data we are expecting
 type LoaderData = {
@@ -16,13 +17,19 @@ export const loader: LoaderFunction = async () => {
 export default function Posts() {
   // const { posts } = useLoaderData<typeof loader>();
   const { posts } = useLoaderData() as LoaderData;
+  const adminUser = useOptionalAdminUser();
+
+  // const user = useOptionalUser();
+  // const isAdmin = user?.email === ENV.ADMIN_EMAIL;
 
   return (
     <main>
       <h1>posts</h1>
-      <Link to="admin" className="text-red-600 underline">
-        Admin
-      </Link>
+      {adminUser ? (
+        <Link to="admin" className="text-red-600 underline">
+          Admin
+        </Link>
+      ) : null}
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
